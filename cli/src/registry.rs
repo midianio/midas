e99e452c3b4b2b5663115e1e6934c93a581a4886
Matt Schroder <matt@midian.io>
2026-08-12 13:39:55 -0500

    chore(release): bump versions to v0.7.2

--- cli/src/registry.rs
+++ cli/src/registry.rs
@@ -23,6 +23,7 @@ const HISTORY: &[(&str, &str)] = &[
     ("0.6.0", include_str!("../../registry/history/0.6.0.json")),
     ("0.7.0", include_str!("../../registry/history/0.7.0.json")),
     ("0.7.1", include_str!("../../registry/history/0.7.1.json")),
+    ("0.7.2", include_str!("../../registry/history/0.7.2.json")),
 ];
 
 #[derive(Debug, Deserialize)]
